08a9b79785ebfc30f6792d0aa6de540ef3362206
liutao <yumeng@sdscm.com>
2020-03-15 23:51:06 +0800

    init project

---
+++ src/main/java/com/mydata/example/example4/MyDataExample4Application.java
@@ -0,0 +1,21 @@
+package com.mydata.example.example4;
+
+import com.mydata.config.MyDataAutoConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MyDataAutoConfig
+public class MyDataExample4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDataExample4Application.class, args);
+        
+        readMe();
+    }
+
+    private static void readMe() {
+        //1 简化Param OrderBy 代码长度
+    }
+
+}
